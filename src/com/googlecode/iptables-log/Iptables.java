364736f456c604a10a553aeced2a3e6067a2061c
pragma- <pragma78@gmail.com>
2011-09-14 08:19:01 +0000

    Added iptable rules for most, hopefully all, cell and wifi interfaces

--- src/com/googlecode/iptables-log/Iptables.java
+++ src/com/googlecode/iptables-log/Iptables.java
@@ -1,19 +1,41 @@
 package com.googlecode.iptableslog;
 
 public class Iptables {
+  public static final String[] CELL_INTERFACES = {
+    "rmnet+", "ppp+", "pdp+"
+  };
+
+  public static final String[] WIFI_INTERFACES = {
+    "eth+", "wlan+", "tiwlan+", "athwlan+"
+  };
+
   public static boolean startLog() {
     if(checkLog() == true)
       return false;
 
-    ShellCommand command = new ShellCommand(new String[] { "su", "-c", "iptables -I OUTPUT 1 -o rmnet+ -j LOG --log-prefix \"[IptablesLogEntry]\" --log-uid" });
-    command.start();
-    while(command.checkForExit() == false)
-      try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+    for(String iface : CELL_INTERFACES) {
+      ShellCommand command = new ShellCommand(new String[] { "su", "-c", "iptables -I OUTPUT 1 -o " + iface + " -j LOG --log-prefix \"[IptablesLogEntry]\" --log-uid" });
+      command.start();
+      while(command.checkForExit() == false)
+        try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
 
-    command = new ShellCommand(new String[] { "su", "-c", "iptables -I INPUT 1 -i rmnet+ -j LOG --log-prefix \"[IptablesLogEntry]\" --log-uid" });
-    command.start();
-    while(command.checkForExit() == false)
-      try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+      command = new ShellCommand(new String[] { "su", "-c", "iptables -I INPUT 1 -i " + iface + " -j LOG --log-prefix \"[IptablesLogEntry]\" --log-uid" });
+      command.start();
+      while(command.checkForExit() == false)
+        try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    for(String iface : WIFI_INTERFACES) {
+      ShellCommand command = new ShellCommand(new String[] { "su", "-c", "iptables -I OUTPUT 1 -o " + iface + " -j LOG --log-prefix \"[IptablesLogEntry]\" --log-uid" });
+      command.start();
+      while(command.checkForExit() == false)
+        try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+
+      command = new ShellCommand(new String[] { "su", "-c", "iptables -I INPUT 1 -i " + iface + " -j LOG --log-prefix \"[IptablesLogEntry]\" --log-uid" });
+      command.start();
+      while(command.checkForExit() == false)
+        try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+    }
 
     return true;
   }
@@ -22,16 +44,29 @@ public class Iptables {
     if(checkLog() == false)
       return false;
 
-    ShellCommand command = new ShellCommand(new String[] { "su", "-c", "iptables -D INPUT 1" });
-    command.start();
-    while(command.checkForExit() == false)
-      try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+    for(String iface : CELL_INTERFACES) {
+      ShellCommand command = new ShellCommand(new String[] { "su", "-c", "iptables -D INPUT 1" });
+      command.start();
+      while(command.checkForExit() == false)
+        try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
 
-    command = new ShellCommand(new String[] { "su", "-c", "iptables -D OUTPUT 1" });
-    command.start();
-    while(command.checkForExit() == false)
-      try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+      command = new ShellCommand(new String[] { "su", "-c", "iptables -D OUTPUT 1" });
+      command.start();
+      while(command.checkForExit() == false)
+        try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+    }
 
+    for(String iface : WIFI_INTERFACES) {
+      ShellCommand command = new ShellCommand(new String[] { "su", "-c", "iptables -D INPUT 1" });
+      command.start();
+      while(command.checkForExit() == false)
+        try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+
+      command = new ShellCommand(new String[] { "su", "-c", "iptables -D OUTPUT 1" });
+      command.start();
+      while(command.checkForExit() == false)
+        try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+    }
     return true;
   }
 
