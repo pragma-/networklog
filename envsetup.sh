603010212746a99d41ed6cdc6aefa1467a40e04e
pragma- <pragma78@gmail.com>
2013-01-25 07:25:58 -0800

    Update envsetup.sh with command to push git tags Separate logcat() into logcat_networklog() and logcat_all()
    Note that both logcat functions will log all entries to logfile, however
    the logcat_networklog() function will filter the visible output to show
    only NetworkLog entries (while still logging all entries to logfile)

--- envsetup.sh
+++ envsetup.sh
@@ -10,7 +10,8 @@ function debug_build() { ant debug; }
 function release_build() { ant release; }
 function install_device() { adb -d install -r bin/$APP; }
 function install_emulator() { adb -e install -r bin/$APP; }
-function logcat() { adb logcat -c && adb logcat -v time | tee log-`date +%Y%m%d-%H:%M.%N` | grep -v GoogleAnalyticsTracker; }
+function logcat_networklog() { adb logcat -c && adb logcat -v time | tee log-`date +%Y%m%d-%H:%M.%N` | grep NetworkLog; }
+function logcat_all() { adb logcat -c && adb logcat -v time | tee log-`date +%Y%m%d-%H:%M.%N`; }
 function meminfo() { adb shell dumpsys meminfo; }
 function procrank_sample() { adb shell procrank | grep networklog | tee procrank-`date +%Y%m%d-%H:%M.%N`; }
 function build_install_and_logcat() { release_build && install_device && logcat; }
@@ -28,6 +29,14 @@ function jdb_debug()
   jdb -sourcepath ./src -connect com.sun.jdi.SocketAttach:hostname=localhost,port=8700
 }
 
+function push_tags()
+{
+  git push github master --tags
+  git push bb master --tags
+  git push gc master --tags
+  git push gitorious master --tags
+}
+
 function push() 
 {
   git push github master
@@ -40,8 +49,10 @@ alias b=build_install_and_logcat
 alias c=clean
 alias id=install_device
 alias ie=install_emulator
-alias lc=logcat
+alias lc=logcat_networklog
+alias lca=logcat_all
 alias mi=meminfo
 alias procrank=procrank_sample
 alias v=check_status
 alias j=jdb_debug
+alias pt=push_tags
