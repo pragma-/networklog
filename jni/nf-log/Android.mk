95f673f12bf38b487489252ff6075dbf99357b68
pragma- <pragma78@gmail.com>
2013-03-11 07:29:35 -0700

    Rename nf-log to nflog Add nflog binary zipfiles

--- jni/nf-log/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := nf-log
-
-LOCAL_SRC_FILES := attr.c callback.c nf-log.c nlmsg.c socket.c
-
-include $(BUILD_EXECUTABLE)
--- jni/nflog/nflog.c
+++ jni/nflog/nflog.c
@@ -1,4 +1,9 @@
-/* This example is placed in the public domain. */
+/* (C) 2012 Pragmatic Software
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
