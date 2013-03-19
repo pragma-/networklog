18e79e91e930c121667b091352a674b6df95e87c
pragma- <pragma78@gmail.com>
2013-03-18 19:29:09 -0700

    Performance optimization for LogEntry.isValid() by caching validation

--- src/com/googlecode/networklog/LogEntry.java
+++ src/com/googlecode/networklog/LogEntry.java
@@ -18,28 +18,41 @@ public class LogEntry {
   int spt;
   int dpt;
   long timestamp;
+  boolean validated;
+  boolean valid;
 
   public boolean isValid() {
+    if(validated) {
+      return valid;
+    }
+
+    validated = true;
     if(StringUtils.contains(in, "{}:=")) {
+      valid = false;
       return false;
     }
 
     if(StringUtils.contains(out, "{}:=")) {
+      valid = false;
       return false;
     }
 
     if(StringUtils.contains(proto, "{}:=")) {
+      valid = false;
       return false;
     }
 
     if(StringUtils.contains(src, "{}:=")) {
+      valid = false;
       return false;
     }
 
     if(StringUtils.contains(dst, "{}:=")) {
+      valid = false;
       return false;
     }
 
+    valid = true;
     return true;
   }
 }
